f432d25032973aa47953a5cb77b6e51e6e1d898e
karimdeif <karimdeif@gmail.com>
2021-09-25 19:13:05 +0200

    Update build-docker-image.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -54,9 +54,9 @@ echo -e "BUILDING CONTAINER IMAGE: ${IMAGE_NAME}:${IMAGE_TAG}"
 #if [ -z "${DOCKER_FILE}" ]; then DOCKER_FILE=${DOCKER_ROOT}/Dockerfile ; fi
 set -x
 ls -l
-cd hibernate-orm-multi-tenancy-quickstart/src/main/docker/
+#cd hibernate-orm-multi-tenancy-quickstart/src/main/docker/
 
-ibmcloud cr build -t ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} -f Dockerfile.jvm
+ibmcloud cr build --file Dockerfile.jvm --tag ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} hibernate-orm-multi-tenancy-quickstart/src/main/docker/
 set +x
 
 ibmcloud cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
