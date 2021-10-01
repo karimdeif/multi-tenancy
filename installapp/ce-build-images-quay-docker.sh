15aabca67df4be55659458b48222fe66acded1ab
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-10-01 17:54:57 +0200

    see last commit

--- installapp/ce-build-images-quay-docker.sh
+++ installapp/ce-build-images-quay-docker.sh
@@ -16,26 +16,26 @@ echo "Path: $ROOT_PATH"
 echo "************************************"
 echo " Clean up container if needed"
 echo "************************************"
-podman image list
-podman container list
-#podman container stop -f  "TBD"
-#podman container rm -f "TBD"
-#podman image prune -a -f
-podman image rm -f "$SERVICE_CATALOG"
-podman image rm -f "$FRONTEND"
+docker image list
+docker container list
+#docker container stop -f  "TBD"
+#docker container rm -f "TBD"
+#docker image prune -a -f
+docker image rm -f "$SERVICE_CATALOG"
+docker image rm -f "$FRONTEND"
 # rm -rf ~/var/home/core/.local/share/containers/storage/overlay/* f
-#podman image rm -f "docker.io/adoptopenjdk/maven-openjdk11"
-#podman image rm -f "docker.io/adoptopenjdk/openjdk11-openj9:ubi-minimal"
-#podman image rm -f "registry.access.redhat.com/ubi8/ubi-minimal"
+#docker image rm -f "docker.io/adoptopenjdk/maven-openjdk11"
+#docker image rm -f "docker.io/adoptopenjdk/openjdk11-openj9:ubi-minimal"
+#docker image rm -f "registry.access.redhat.com/ubi8/ubi-minimal"
 
 echo "************************************"
 echo " Service catalog $SERVICE_CATALOG"
 echo "************************************"
 cd $ROOT_PATH/code/service-catalog-tmp
 pwd
-podman login quay.io
-podman build -t "quay.io/$REPOSITORY/$SERVICE_CATALOG" -f Dockerfile.simple .
-# podman push "quay.io/$REPOSITORY/$SERVICE_CATALOG"
+docker login quay.io
+docker build -t "quay.io/$REPOSITORY/$SERVICE_CATALOG" -f Dockerfile.simple .
+# docker push "quay.io/$REPOSITORY/$SERVICE_CATALOG"
 
 echo ""
 
@@ -44,6 +44,6 @@ echo " Frontend $FRONTEND"
 echo "************************************"
 cd $ROOT_PATH/code/frontend
 #pwd
-# podman login quay.io
-# podman build -t "quay.io/$REPOSITORY/$FRONTEND" -f Dockerfile.os4-webapp .
-# podman push "quay.io/$REPOSITORY/$FRONTEND"
+# docker login quay.io
+# docker build -t "quay.io/$REPOSITORY/$FRONTEND" -f Dockerfile.os4-webapp .
+# docker push "quay.io/$REPOSITORY/$FRONTEND"
