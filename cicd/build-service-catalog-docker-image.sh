6eb9a884b610a8ce8f4772639b4e56bf2f47fad6
karimdeif <karimdeif@gmail.com>
2021-10-06 14:39:57 +0200

    moved cert stuff from build to deploy

--- cicd/build-service-catalog-docker-image.sh
+++ cicd/build-service-catalog-docker-image.sh
@@ -20,80 +20,6 @@ echo "GIT_COMMIT=${GIT_COMMIT}"
 echo "DOCKER_ROOT=${DOCKER_ROOT}"
 echo "DOCKER_FILE=${DOCKER_FILE}"
 
-export default_datasource_base_certs="${default_datasource_base_certs:=5df929c2-b76a-11e9-b3dd-4acf6c229d45}"
-#echo "default_datasource_base_certs=${default_datasource_base_certs}"
-
-export default_datasource_mycompany_certs="${default_datasource_mycompany_certs:=2b11af40-8aa6-4b13-a424-1a9109624264}"
-#echo "default_datasource_mycompany_certs=${default_datasource_mycompany_certs}"
-
-export default_datasource_certs_data="${default_datasource_certs_data:=-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
-                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
-                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
-                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
-                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
-                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
-                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
-                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
-                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
-                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
-                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
-                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
-                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
-                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
-                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
-                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
-                                        }"
-
-#echo "default_datasource_certs_data=${default_datasource_certs_data}"
-
-export default_datasource_base_certs_data="${default_datasource_base_certs_data:-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
-                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
-                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
-                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
-                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
-                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
-                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
-                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
-                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
-                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
-                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
-                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
-                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
-                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
-                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
-                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
-                                        }"
-
-#echo "default_datasource_base_certs_data=${default_datasource_base_certs_data}"
-
-
-export "default_datasource_mycompany_certs_data=${default_datasource_mycompany_certs_data:-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
-                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
-                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
-                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
-                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
-                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
-                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
-                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
-                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
-                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
-                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
-                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
-                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
-                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
-                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
-                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
-
-                                        }"
 
 #echo "default_datasource_mycompany_certs_data=${default_datasource_mycompany_certs_data}"
 
@@ -117,14 +43,13 @@ set -x
 
 #ibmcloud cr build --file Dockerfile --tag ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} .
 
+                    #--build-arg default.datasource.base.certs=${default_datasource_base_certs} \
+                    #--build-arg default.datasource.mycompany.certs=${default_datasource_mycompany_certs} \
+                    #--build-arg default.datasource.certs.data=\'$default_datasource_certs_data\' \
+                    #--build-arg default.datasource.base.certs.data=\'$default_datasource_base_certs_data\' \
+                    #--build-arg default.datasource.mycompany.certs.data=\'$default_datasource_mycompany_certs_data\' \
                 
-ibmcloud cr build   --file Dockerfile \
-                    --build-arg default.datasource.base.certs=${default_datasource_base_certs} \
-                    --build-arg default.datasource.mycompany.certs=${default_datasource_mycompany_certs} \
-                    --build-arg default.datasource.certs.data=\'$default_datasource_certs_data\' \
-                    --build-arg default.datasource.base.certs.data=\'$default_datasource_base_certs_data\' \
-                    --build-arg default.datasource.mycompany.certs.data=\'$default_datasource_mycompany_certs_data\' \
-                    --tag ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest .
+ibmcloud cr build  --file Dockerfile --tag ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest .
                           
 set +x
 
--- cicd/deploy-service-catalog-to-ce.sh
+++ cicd/deploy-service-catalog-to-ce.sh
@@ -15,44 +15,115 @@ echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
 echo "DEPLOYMENT_FILE=${DEPLOYMENT_FILE}"
 
 
-echo "default.datasource.certs=${default.datasource.certs:5df929c2-b76a-11e9-b3dd-4acf6c229d45}"
-echo "default.datasource.username=${default.datasource.username:ibm_cloud_a66ff784_d8a6_4545_bb4c_b24dec4e02b8}"
-echo "default.datasource.password=${default.datasource.password:7f5dfe3c8d3eabcf52d15a7b81ce845a013e681ed753e063a4be3276a4461530}"
-echo "default.datasource.jdbc.url=${default.datasource.jdbc.url:jdbc:postgresql://3bd53622-20ce-426b-8232-cf6a3d0c52ce.bkvfv1ld0bj2bdbncbeg.databases.appdomain.cloud:30143/ibmclouddb}"
+#echo "default.datasource.certs=${default.datasource.certs:5df929c2-b76a-11e9-b3dd-4acf6c229d45}"
 
-echo "default.datasource.base.certs=${default.datasource.base.certs:5df929c2-b76a-11e9-b3dd-4acf6c229d45}"
-echo "default.datasource.base.username=${default.datasource.base.username:ibm_cloud_a66ff784_d8a6_4545_bb4c_b24dec4e02b8}"
-echo "default.datasource.base.password=${default.datasource.base.password:7f5dfe3c8d3eabcf52d15a7b81ce845a013e681ed753e063a4be3276a4461530}"
-echo "default.datasource.base.jdbc.url=${default.datasource.base.jdbc.url:jdbc:postgresql://3bd53622-20ce-426b-8232-cf6a3d0c52ce.bkvfv1ld0bj2bdbncbeg.databases.appdomain.cloud:30143/ibmclouddb}"
+export default_datasource_username=${default_datasource_username:ibm_cloud_a66ff784_d8a6_4545_bb4c_b24dec4e02b8}
+export default_datasource_password=${default_datasource_password:7f5dfe3c8d3eabcf52d15a7b81ce845a013e681ed753e063a4be3276a4461530}
+export default_datasource_jdbc_url=${default_datasource_jdbc_url:jdbc:postgresql://3bd53622-20ce-426b-8232-cf6a3d0c52ce.bkvfv1ld0bj2bdbncbeg.databases.appdomain.cloud:30143/ibmclouddb}
 
-echo "default.datasource.mycompany.certs=${default.datasource.mycompany.certs:2b11af40-8aa6-4b13-a424-1a9109624264}"
-echo "default.datasource.mycompany.username=${default.datasource.mycompany.username:ibm_cloud_27df2776_ca1d_4e5d_acc8_79e557a60482}"
-echo "default.datasource.mycompany.password=${default.datasource.mycompany.password:1381b9739602be4254a0dedd56a5093f1d641414b3714643c3a8003b10efe146}"
-echo "default.datasource.mycompany.jdbc.url=${default.datasource.mycompany.jdbc.url:jdbc:postgresql://5e6b66a4-70b6-4caf-be8b-23cd2d1ed26b.c00no9sd0hobi6kj68i0.databases.appdomain.cloud:30266/ibmclouddb}"
+export default_datasource_base_username=${default_datasource_base_username:ibm_cloud_a66ff784_d8a6_4545_bb4c_b24dec4e02b8}
+export default_datasource_base_password=${default_datasource_base_password:7f5dfe3c8d3eabcf52d15a7b81ce845a013e681ed753e063a4be3276a4461530}
+export default_datasource_base_jdbc.url=${default_datasource_base_jdbc.url:jdbc:postgresql://3bd53622-20ce-426b-8232-cf6a3d0c52ce.bkvfv1ld0bj2bdbncbeg.databases.appdomain.cloud:30143/ibmclouddb}
+
+export default_datasource_mycompany_username=${default_datasource_mycompany_username:ibm_cloud_27df2776_ca1d_4e5d_acc8_79e557a60482}
+export default_datasource_mycompany_password=${default_datasource_mycompany_password:1381b9739602be4254a0dedd56a5093f1d641414b3714643c3a8003b10efe146}
+export default_datasource_mycompany_jdbc_url="jdbc\:${default.datasource.mycompany.jdbc.url:jdbc:postgresql://5e6b66a4-70b6-4caf-be8b-23cd2d1ed26b.c00no9sd0hobi6kj68i0.databases.appdomain.cloud:30266/ibmclouddb}"
+
+export default_datasource_base_certs=${default_datasource_base_certs:5df929c2-b76a-11e9-b3dd-4acf6c229d45}
+
+export default_datasource_mycompany_certs=${default_datasource_mycompany_certs:=2b11af40-8aa6-4b13-a424-1a9109624264}
+
+
+export default_datasource_certs_data="${default_datasource_certs_data:=-----BEGIN CERTIFICATE-----
+                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
+                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
+                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
+                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
+                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
+                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
+                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
+                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
+                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
+                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
+                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
+                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
+                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
+                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
+                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
+                                                                    -----END CERTIFICATE-----
+                                        }"
+
+export default_datasource_base_certs_data="${default_datasource_base_certs_data:-----BEGIN CERTIFICATE-----
+                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
+                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
+                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
+                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
+                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
+                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
+                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
+                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
+                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
+                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
+                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
+                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
+                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
+                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
+                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
+                                                                    -----END CERTIFICATE-----
+                                        }"
+
+
+export "default_datasource_mycompany_certs_data=${default_datasource_mycompany_certs_data:-----BEGIN CERTIFICATE-----
+                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
+                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
+                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
+                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
+                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
+                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
+                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
+                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
+                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
+                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
+                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
+                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
+                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
+                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
+                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
+                                                                    -----END CERTIFICATE-----
+
+                                        }"
+
 
 set -x
-#kubectl apply --namespace ${CLUSTER_NAMESPACE} -f ${DEPLOYMENT_FILE} 
 
 ibmcloud plugin install code-engine
 ibmcloud resource groups
 ibmcloud target -g Default
 ibmcloud ce project select --name multi-tenant
-#ibmcloud ce registry create --name ibm-container-registry --server us.icr.io --username iamapikey --password $API
 
 ibmcloud ce application create --name service-catalog-a --image us.icr.io/multi-tenancy-cr/service-catalog:latest \
                                                         --port 8081 --rs test \
-                                                        --env default.datasource.certs=${default.datasource.certs} \
-                                                        --env default.datasource.username=${default.datasource.username} \
-                                                        --env default.datasource.password=${default.datasource.password} \
-                                                        --env default.datasource.jdbc.url=${default.datasource.jdbc.url} \
-                                                        --env default.datasource.base.certs=${default.datasource.base.certs} \
-                                                        --env default.datasource.base.username=${default.datasource.base.username} \
-                                                        --env default.datasource.base.password=${default.datasource.base.password} \
-                                                        --env default.datasource.base.jdbc.url=${default.datasource.base.jdbc.url} \
-                                                        --env default.datasource.mycompany.certs=${default.datasource.mycompany.certs} \
-                                                        --env default.datasource.mycompany.username=${default.datasource.mycompany.username} \
-                                                        --env default.datasource.mycompany.password=${default.datasource.mycompany.password} \
-                                                        --env default.datasource.mycompany.jdbc.url=${default.datasource.mycompany.jdbc.url}
+                                                        --env default.datasource.certs=${default_datasource_certs} \
+                                                        --env default.datasource.certs.data=${default_datasource_certs_data} \
+                                                        --env default.datasource.username=${default_datasource_username} \
+                                                        --env default.datasource.password=${default_datasource_password} \
+                                                        --env default.datasource.jdbc.url=${default_datasource_jdbc_url} \
+                                                        --env default.datasource.base.certs=${default_datasource_base_certs} \
+                                                        --env default.datasource.base.certs.data=${default_datasource_base_certs_data} \
+                                                        --env default.datasource.base.username=${default_datasource_base_username} \
+                                                        --env default.datasource.base.password=${default_datasource_base_password} \
+                                                        --env default.datasource.base.jdbc.url=${default_datasource_base_jdbc_url} \
+                                                        --env default.datasource.mycompany.certs=${default_datasource_mycompany_certs} \
+                                                        --env default.datasource.mycompany.certs.data=${default_datasource_mycompany_certs_data} \
+                                                        --env default.datasource.mycompany.username=${default_datasource_mycompany_username} \
+                                                        --env default.datasource.mycompany.password=${default_datasource_mycompany_password} \
+                                                        --env default.datasource.mycompany.jdbc.url=${default_datasource_mycompany_jdbc_url}
 
 set +x
 
