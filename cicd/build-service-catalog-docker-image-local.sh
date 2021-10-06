4432db98c72dd6d0c43b2bea0126afcd884cebf0
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-10-06 15:34:17 +0200

    update scripts and dockerfile

--- cicd/build-service-catalog-docker-image-local.sh
+++ cicd/build-service-catalog-docker-image-local.sh
@@ -7,10 +7,9 @@ export IMAGE_NAME=service_catalog_local
 export IMAGE_TAG=v1
 
 # default
-export default_datasource_base_certs="${default_datasource_base_certs:=5df929c2-b76a-11e9-b3dd-4acf6c229d45}"
-export default_datasource_mycompany_certs="${default_datasource_mycompany_certs:=2b11af40-8aa6-4b13-a424-1a9109624264}"
-export default_datasource_certs_data="${default_datasource_certs_data:=-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+export default_datasource_base_certs="5df929c2-b76a-11e9-b3dd-4acf6c229d45"
+export default_datasource_mycompany_certs="2b11af40-8aa6-4b13-a424-1a9109624264"
+export default_datasource_certs_data="MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
                                                                     BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
                                                                     MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
                                                                     hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
@@ -26,32 +25,9 @@ export default_datasource_certs_data="${default_datasource_certs_data:=-----BEGI
                                                                     qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
                                                                     doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
                                                                     i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
-                                        }"
-export default_datasource_base_certs_data="${default_datasource_base_certs_data:-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
-                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
-                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
-                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
-                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
-                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
-                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
-                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
-                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
-                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
-                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
-                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
-                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
-                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
-                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
-                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
-                                        }"
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA=="
 
-export "default_datasource_mycompany_certs_data=${default_datasource_mycompany_certs_data:-----BEGIN CERTIFICATE-----
-                                                                    MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+export default_datasource_base_certs_data="MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
                                                                     BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
                                                                     MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
                                                                     hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
@@ -67,10 +43,25 @@ export "default_datasource_mycompany_certs_data=${default_datasource_mycompany_c
                                                                     qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
                                                                     doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
                                                                     i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
-                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA==
-                                                                    -----END CERTIFICATE-----
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA=="
 
-                                        }"
+export "default_datasource_mycompany_certs_data=MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
+                                                                    BAMME0lCTSBDbG91ZCBEYXRhYmFzZXMwHhcNMTgwNjI1MTQyOTAwWhcNMjgwNjIy
+                                                                    MTQyOTAwWjAeMRwwGgYDVQQDDBNJQk0gQ2xvdWQgRGF0YWJhc2VzMIIBIjANBgkq
+                                                                    hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8lpaQGzcFdGqeMlmqjffMPpIQhqpd8qJ
+                                                                    Pr3bIkrXJbTcJJ9uIckSUcCjw4Z/rSg8nnT13SCcOl+1to+7kdMiU8qOWKiceYZ5
+                                                                    y+yZYfCkGaiZVfazQBm45zBtFWv+AB/8hfCTdNF7VY4spaA3oBE2aS7OANNSRZSK
+                                                                    pwy24IUgUcILJW+mcvW80Vx+GXRfD9Ytt6PRJgBhYuUBpgzvngmCMGBn+l2KNiSf
+                                                                    weovYDCD6Vngl2+6W9QFAFtWXWgF3iDQD5nl/n4mripMSX6UG/n6657u7TDdgkvA
+                                                                    1eKI2FLzYKpoKBe5rcnrM7nHgNc/nCdEs5JecHb1dHv1QfPm6pzIxwIDAQABo1Aw
+                                                                    TjAdBgNVHQ4EFgQUK3+XZo1wyKs+DEoYXbHruwSpXjgwHwYDVR0jBBgwFoAUK3+X
+                                                                    Zo1wyKs+DEoYXbHruwSpXjgwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
+                                                                    AQEAJf5dvlzUpqaix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ
+                                                                    lV5P0N2xwuSl9ZqAJt7/k/3ZeB+nYwPoyO3KvKvATunRvlPBn4FWVXeaPsG+7fhS
+                                                                    qsejmkyonYw77HRzGOzJH4Zg8UN6mfpbaWSsyaExvqknCp9SoTQP3D67AzWqb1zY
+                                                                    doqqgGIZ2nxCkp5/FXxF/TMb55vteTQwfgBy60jVVkbF7eVOWCv0KaNHPF5hrqbN
+                                                                    i+3XjJ7/peF3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz
+                                                                    +PQzp/B9cQmOO8dCe049Q3oaUA=="
 
 # **********************************************************************************
 # Execution
@@ -83,7 +74,24 @@ set -x
 cd ../code/service-catalog-tmp
 
 # Build the image
-docker build  --file Dockerfile \
-              --tag "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG" .
+#docker build  --file Dockerfile \
+#              --tag "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG" .
+
+docker run --name="service-catalog-verification" \
+           -it \
+           --env default.datasource.certs=${default_datasource_certs} \
+           --env default.datasource.certs.data=${default_datasource_certs_data} \
+           --env default.datasource.username=${default_datasource_username} \
+           --env default.datasource.password=${default_datasource_password} \
+           --env default.datasource.jdbc.url=${default_datasource_jdbc_url} \
+           --env default.datasource.base.certs=${default_datasource_base_certs} \
+           --env default.datasource.base.certs.data=${default_datasource_base_certs_data} \
+           --env default.datasource.base.username=${default_datasource_base_username} \
+           --env default.datasource.base.password=${default_datasource_base_password} \
+           --env default.datasource.base.jdbc.url=${default_datasource_base_jdbc_url} \
+           --env default.datasource.mycompany.certs=${default_datasource_mycompany_certs} \
+           --env default.datasource.mycompany.certs.data=${default_datasource_mycompany_certs_data} \
+           -p 8080:8080/tcp \
+           "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
                            
 
