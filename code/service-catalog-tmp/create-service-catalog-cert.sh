6469b54079c1a2d62f747026b971af1633033ea4
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-10-06 15:45:02 +0200

    update scripts for service catalog

---
+++ code/service-catalog-tmp/create-service-catalog-cert.sh
@@ -0,0 +1,2 @@
+# Verify the variable in the 'docker_entry-sample.sh file'
+cat ${default.datasource.certs} > $CERT_FILE_NAME
--- code/service-catalog-tmp/docker-service-catalog-entry.sh
+++ code/service-catalog-tmp/docker-service-catalog-entry.sh
@@ -5,12 +5,15 @@ export info=$(pwd)
 echo "**********************************"
 echo "-> Log: Root path: '$info'"
 echo "-> Log: Check env variables:" 
-echo " '$POSTGRES_1'"
-echo " '$POSTGRES_2'"
+echo " '$default.datasource.certs'"
+echo " '$default.datasource.certs.data'"
 echo " '$POSTGRES_cert_content'"
 
-export CERT_FILE_NAME=outside_location/mycert
-"/bin/sh" ./create-cert.sh
+export CERT_FILE_NAME=./outside_location/mycert
+"/bin/sh" ./create-service-catalog-cert.sh
+more ./create-service-catalog-cert.sh
+more ./outside_location/mycert
+
 
 echo "**********************************"
 echo "Execute java command "
@@ -22,9 +25,18 @@ java -Xmx128m \
      -Xtune:virtualized \
      -Xscmx128m \
      -Xshareclasses:cacheDir=/opt/shareclasses \
-     -D_POSTGRES_1=${POSTGRES_1} \
-     -D_POSTGRES_2=${POSTGRES_2} \
-     -D_CERT_FILE_NAME=${CERT_FILE_NAME} \
-     - cp D_CERT_FILE_NAME \
      -jar \
      /deployments/quarkus-run.jar
+
+#java -Xmx128m \
+#     -Xscmaxaot100m \
+#     -XX:+IdleTuningGcOnIdle \
+#     -Xtune:virtualized \
+#     -Xscmx128m \
+#     -Xshareclasses:cacheDir=/opt/shareclasses \
+#     -D_POSTGRES_1=${POSTGRES_1} \
+#     -D_POSTGRES_2=${POSTGRES_2} \
+#     -D_CERT_FILE_NAME=${CERT_FILE_NAME} \
+#     - cp D_CERT_FILE_NAME \
+#     -jar \
+#     /deployments/quarkus-run.jar
