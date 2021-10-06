99528d943bd4b42e66baa578bbfa91f58937c316
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-10-06 16:23:49 +0200

    update script

--- cicd/build-service-catalog-docker-image-local.sh
+++ cicd/build-service-catalog-docker-image-local.sh
@@ -9,11 +9,6 @@ export IMAGE_TAG=v1
 # default
 export default_datasource_base_certs="5df929c2-b76a-11e9-b3dd-4acf6c229d45"
 export default_datasource_mycompany_certs="2b11af40-8aa6-4b13-a424-1a9109624264"
-export default_datasource_certs_data="MIIDDzCCAfegAwIBAgIJANEH58y2/F3xMvTMoy35DcT3E2ZeSVjouZs15O90kI3k2daS2OHJABW0vSj4nLz+PQzp/B9cQmOO8dCe049Q3oaUA=="
-
-export default_datasource_base_certs_data="MIIDDzCCAfegAwIBAgIJANEH58y2/kzHMA0GCSqGSIb3DQEBCwUAMB4xHDAa5zB=="
-
-export "default_datasource_mycompany_certs_data=MIIDDzCCAfegAwIBAgIJANix26qJEuqFG0IP57QQI5TCRJ6Xt/supRHo63eDvKw8zR7tlWQ9Q3oaUA=="
 
 # **********************************************************************************
 # Execution
@@ -36,16 +31,6 @@ docker run --name="service-catalog-verification" \
            -it \
            --env default_datasource_certs=${default_datasource_base_certs} \
            --env default_datasource_certs_data=${default_datasource_certs_data} \
-           --env default.datasource.username=${default_datasource_username} \
-           --env default.datasource.password=${default_datasource_password} \
-           --env default.datasource.jdbc.url=${default_datasource_jdbc_url} \
-           --env default.datasource.base.certs=${default_datasource_base_certs} \
-           --env default.datasource.base.certs.data=${default_datasource_base_certs_data} \
-           --env default.datasource.base.username=${default_datasource_base_username} \
-           --env default.datasource.base.password=${default_datasource_base_password} \
-           --env default.datasource.base.jdbc.url=${default_datasource_base_jdbc_url} \
-           --env default.datasource.mycompany.certs=${default_datasource_mycompany_certs} \
-           --env default.datasource.mycompany.certs.data=${default_datasource_mycompany_certs_data} \
            -p 8080:8080/tcp \
            "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
                            
